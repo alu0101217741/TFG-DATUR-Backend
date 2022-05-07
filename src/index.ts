e6fa0ecdaeceea3f353fba85a79afd33fc8ad5a6
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2022-05-08 00:57:35 +0100

    refactor: deleted some files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.listen(app.get('port'), () => {
-  console.log('Server is running on port ' + app.get('port'));
-});
