924f17c8432ceb8477d502e57d6095cbbf25728b
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2022-07-21 00:26:52 +0100

    feat: new time cron job

--- src/data-processor/app/clock.ts
+++ src/data-processor/app/clock.ts
@@ -2,7 +2,7 @@ import { CronJob } from 'cron'
 import { startDataProcessor } from './worker'
 
 const cronJob = new CronJob({
-  cronTime: '0 */30 * * * *',
+  cronTime: '0 */15 * * * *',
   onTick: async () => {
     await startDataProcessor()
   },
